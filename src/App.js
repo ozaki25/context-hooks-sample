cc72b556f2533a955c53a8dfdbb202a9eb425db2
ozaki25 <fs65ugumori@yahoo.co.jp>
2019-12-20 17:03:27 +0900

    use context api

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import React from 'react';
+import React, { createContext, useContext } from 'react';
+
+const TodoContext = createContext(null);
+
+function TodoProvider(props) {
+  return <TodoContext.Provider value={{ hello: '!!!!!!!' }} {...props} />;
+}
+
+function Hello() {
+  const { hello } = useContext(TodoContext);
+  return <h1>{hello}</h1>;
+}
 
 function App() {
   return (
-    <div>
-      <h1>Hello</h1>
-    </div>
+    <TodoProvider>
+      <Hello />
+    </TodoProvider>
   );
 }
 
