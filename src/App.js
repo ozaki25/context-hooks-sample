9d529d04d217148019a0fe359aa225889cebcf21
ozaki25 <fs65ugumori@yahoo.co.jp>
2019-12-20 17:05:00 +0900

    update

--- src/App.js
+++ src/App.js
@@ -3,22 +3,22 @@ import React, { createContext, useContext } from 'react';
 const TodoContext = createContext(null);
 
 function TodoProvider(props) {
-  return <TodoContext.Provider value={{ hello: '!!!!!!!' }} {...props} />;
+  return <TodoContext.Provider value={{ todo: '!!!!!!!' }} {...props} />;
 }
 
 function useTodoContext() {
   return useContext(TodoContext);
 }
 
-function Hello() {
-  const { hello } = useTodoContext();
-  return <h1>{hello}</h1>;
+function Todo() {
+  const { todo } = useTodoContext();
+  return <h1>{todo}</h1>;
 }
 
 function App() {
   return (
     <TodoProvider>
-      <Hello />
+      <Todo />
     </TodoProvider>
   );
 }
